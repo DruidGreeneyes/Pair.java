dbe7a97a060bd6b2f1bfc121a62652abcc04e47a
JBox <josh.carton@gmail.com>
2016-10-05 18:58:50 +0100

    re-implement UniformPair.F things

--- src/pair/UniformPair.java
+++ src/pair/UniformPair.java
@@ -1,6 +1,13 @@
 package pair;
 
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.ToDoubleBiFunction;
+import java.util.function.ToDoubleFunction;
 import java.util.function.UnaryOperator;
 
 public class UniformPair<T> implements ImmutablePair<T, T> {
@@ -49,6 +56,10 @@ public class UniformPair<T> implements ImmutablePair<T, T> {
         return new UniformPair<>(left, fun.apply(right));
     }
 
+    public UniformPair<T> map(UnaryOperator<UniformPair<T>> fun) {
+        return fun.apply(this);
+    }
+
     public <R> Pair<R, T> replaceLeft(final R neoLiberal) {
         return new Pair<>(neoLiberal, right);
     }
@@ -74,6 +85,66 @@ public class UniformPair<T> implements ImmutablePair<T, T> {
         public static final <T> Function<T, UniformPair<T>> partialLeft(
                 final T left) {
             return right -> new UniformPair<>(left, right);
+        }
+
+        public static final <T, R> Function<UniformPair<T>, Pair<R, T>> mapLeft(
+                final Function<T, R> fun) {
+            return pair -> pair.mapLeft(fun);
+        }
+
+        public static final <T, R> Function<UniformPair<T>, Pair<T, R>> mapRight(
+                final Function<T, R> fun) {
+            return pair -> pair.mapRight(fun);
+        }
+
+        public static final <T, R> Function<UniformPair<T>, Pair<R, T>> replaceLeft(
+                final R neoLiberal) {
+            return pair -> pair.replaceLeft(neoLiberal);
+        }
+
+        public static final <T, R> Function<UniformPair<T>, Pair<T, R>> replaceRight(
+                final R altRight) {
+            return pair -> pair.replaceRight(altRight);
+        }
+
+        public static final <T, R> Function<UniformPair<T>, R> intoFun(
+                final BiFunction<T, T, R> fun) {
+            return pair -> pair.intoFun(fun);
+        }
+
+        public static final <T> UnaryOperator<UniformPair<T>> map(
+                final UnaryOperator<UniformPair<T>> fun) {
+            return pair -> pair.map(fun);
+        }
+
+        public static final <T> ToDoubleFunction<UniformPair<T>> intoDouble(
+                final ToDoubleFunction<ImmutablePair<T, T>> fun) {
+            return pair -> pair.intoDouble(fun);
+        }
+
+        public static final <T> ToDoubleFunction<UniformPair<T>> intoDouble(
+                final ToDoubleBiFunction<T, T> fun) {
+            return pair -> pair.intoDouble(fun);
+        }
+
+        public static final <T> Consumer<UniformPair<T>> intoCon(
+                final BiConsumer<T, T> fun) {
+            return pair -> pair.intoCon(fun);
+        }
+
+        public static final <T> Predicate<UniformPair<T>> intoPred(
+                final BiPredicate<T, T> pred) {
+            return pair -> pair.intoPred(pred);
+        }
+
+        public static final <T> Predicate<UniformPair<T>> predLeft(
+                final Predicate<T> pred) {
+            return pair -> pair.predLeft(pred);
+        }
+
+        public static final <T> Predicate<UniformPair<T>> predRight(
+                final Predicate<T> pred) {
+            return pair -> pair.predRight(pred);
         }
     }
 }
