5d5c42631616b61401013bc02a523350c0d8c902
JBox <josh.carton@gmail.com>
2016-10-16 21:44:29 +0100

    add element mapping functions

--- src/triple/ImmutableTriple.java
+++ src/triple/ImmutableTriple.java
@@ -1,5 +1,8 @@
 package triple;
 
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
 public interface ImmutableTriple<A, B, C> {
     A left();
 
@@ -8,4 +11,16 @@ public interface ImmutableTriple<A, B, C> {
     C right();
 
     String toString();
+
+    <R> ImmutableTriple<R, B, C> mapLeft(Function<A, R> fun);
+
+    ImmutableTriple<A, B, C> mapLeft(UnaryOperator<A> fun);
+
+    <R> ImmutableTriple<A, R, C> mapCenter(Function<B, R> fun);
+
+    ImmutableTriple<A, B, C> mapCenter(UnaryOperator<B> fun);
+
+    <R> ImmutableTriple<A, B, R> mapRight(Function<C, R> fun);
+
+    ImmutableTriple<A, B, C> mapRight(UnaryOperator<C> fun);
 }
--- src/triple/Triple.java
+++ src/triple/Triple.java
@@ -1,5 +1,8 @@
 package triple;
 
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
 public class Triple<A, B, C> implements ImmutableTriple<A, B, C> {
 
     public final A left;
@@ -25,6 +28,36 @@ public class Triple<A, B, C> implements ImmutableTriple<A, B, C> {
     @Override
     public C right() {
         return right;
+    }
+
+    @Override
+    public <R> ImmutableTriple<R, B, C> mapLeft(Function<A, R> fun) {
+        return new Triple<>(fun.apply(left), center, right);
+    }
+
+    @Override
+    public ImmutableTriple<A, B, C> mapLeft(UnaryOperator<A> fun) {
+        return new Triple<>(fun.apply(left), center, right);
+    }
+
+    @Override
+    public <R> ImmutableTriple<A, R, C> mapCenter(Function<B, R> fun) {
+        return new Triple<>(left, fun.apply(center), right);
+    }
+
+    @Override
+    public ImmutableTriple<A, B, C> mapCenter(UnaryOperator<B> fun) {
+        return new Triple<>(left, fun.apply(center), right);
+    }
+
+    @Override
+    public <R> ImmutableTriple<A, B, R> mapRight(Function<C, R> fun) {
+        return new Triple<>(left, center, fun.apply(right));
+    }
+
+    @Override
+    public ImmutableTriple<A, B, C> mapRight(UnaryOperator<C> fun) {
+        return new Triple<>(left, center, fun.apply(right));
     }
 
 }
